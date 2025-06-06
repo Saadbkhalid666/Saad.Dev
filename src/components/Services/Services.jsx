9dccbb45b752860285ac29799d8157f2d19bace9
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-06-06 12:37:45 +0500

    Modifying Services Section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaStoreAlt, FaBriefcase, FaPaperPlane, FaBlog, FaUserAlt } from "react-icons/fa";
+import "./services.css"
 
 export const Services = () => {
   const services = [
@@ -46,18 +47,24 @@ export const Services = () => {
         💼 My Services
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {services.map((service, index) => (
+       <div
+        className="services-container grid grid-cols-1 sm:grid-cols-2 place-items-center  gap-3"
+        
+      >
+        {services.map(({icon,title,description,delay}, index) => (
           <div
-            key={index}
-            className="service-card bg-black card-box p-6 rounded-lg shadow-lg text-center"
-            data-aos="fade-up"
-            data-aos-delay={service.delay}
-          >
-            {service.icon}
-            <h3 className="text-xl font-semibold text-[#ffff70]">{service.title}</h3>
-            <p className="text-gray-300 mt-4">{service.description}</p>
-          </div>
+  key={index}
+  className="card-box flex justify-center items-center p-6 w-full sm:w-1/2 md:w-1/3  "
+  data-aos="fade-up"
+  data-aos-delay={delay}
+>
+  <div className="card-content hover:scale-110 transition-all duration-500 bg-black rounded-lg shadow-2xl p-6 text-center relative z-10 w-full h-full flex flex-col items-center justify-center">
+    {icon}
+    <h3 className="text-white font-semibold text-lg mb-2">{title}</h3>
+    <p className="text-gray-300 text-sm">{description}</p>
+  </div>
+</div>
+
         ))}
       </div>
     </section>
