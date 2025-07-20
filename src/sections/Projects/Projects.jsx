f36f44e34017668ec819bfd0e10cd1e8f6e93454
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-07-20 16:36:34 +0500

    Projects Section Updated

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,7 +3,9 @@ import mrsenterprise from "../../assets/images/mrsenterprise.PNG";
 import snr from "../../assets/images/snr.PNG";
 import { FaProjectDiagram } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
-
+import chatwat from "../../assets/images/chatwat.PNG";
+import { FaGithub } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 export const Projects = () => {
   const cardsItems = [
@@ -11,19 +13,42 @@ export const Projects = () => {
       title: "KS TRADERS",
       category: "Web Development",
       href: "https://kstraders.vercel.app/",
+      skills: ["react", "tailwind", "nodejs", "expressjs", "nodemailer"],
       url: kstraders,
+      github: "https://github.com/Saadbkhalid666/KS-Traders",
     },
     {
       title: "MRS ENTERPRISES",
       category: "Web Development",
       href: "https://mrsenterprise.vercel.app/",
       url: mrsenterprise,
+      skills: ["react", "tailwind"],
+      github: "https://github.com/Saadbkhalid666/m.r.s-enterprise",
     },
     {
       title: "SNR",
       category: "Web Development",
       href: "https://snrhub.vercel.app/",
       url: snr,
+      skills: ["react", "tailwind"],
+      github: "https://github.com/Saadbkhalid666/snr",
+    },
+    {
+      title: "ChatWat",
+      category: "Web Development",
+      href: "https://chatwatapp.vercel.app",
+      url: chatwat,
+      skills: [
+        "react",
+        "tailwind",
+        "nodejs",
+        "expressjs",
+        "mongodb",
+        "socket.io",
+        "jwt-authentication",
+        "nodemailer",
+      ],
+      github: "https://github.com/Saadbkhalid666/chatwat",
     },
   ];
 
@@ -35,32 +60,43 @@ export const Projects = () => {
       </h1>
 
       <div className="projects-cards-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {cardsItems.map(({ title, category, href, url }, index) => (
-          <div
-  className="container group relative bg-black text-white rounded-lg overflow-hidden shadow-lg hover:scale-105 transform transition duration-300"
-  key={index}
->
-  <a href={href} target="_blank" rel="noopener noreferrer" className="block relative">
-    <img
-      src={url}
-      alt={title}
-      className="w-full h-48 object-cover"
-    />
-    <div className="p-4">
-      <h2 className="text-xl font-bold text-[#ffff70] mb-2">
-        {title}
-      </h2>
-      <p className="text-gray-300 text-sm">{category}</p>
-    </div>
-
-    {/* Eye icon */}
-    <span className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] opacity-0 group-hover:opacity-100 text-[#ffff70] text-2xl  transition duration-300 ease-in-out bg-black/50 w-[100%] h-[100%] flex justify-center items-center ">
-      <FaEye />
-    </span>
-  </a>
-</div>
-
-        ))}
+        {cardsItems.map(
+          ({ title, category, href, url, skills, github }, index) => (
+            <div
+              className="container group relative bg-black text-white rounded-lg overflow-hidden shadow-lg hover:scale-105 transform transition duration-300"
+              key={index}
+            >
+              <img src={url} alt={title} className="w-full h-48 object-cover" />
+              <div className="p-4">
+                <h2 className="text-xl font-bold text-[#ffff70] mb-2">
+                  {title}
+                </h2>
+                <p className="text-gray-300 text-sm">{category}</p>
+                <p className="flex flex-wrap gap-2">
+                  {skills.map((skill, index) => (
+                    <span
+                      key={index}
+                      className="bg-blue-400 text-white px-3 py-1 rounded-full text-sm"
+                    >
+                      {skill}
+                    </span>
+                  ))}
+                </p>
+              </div>
+              <div className="link-container flex flex-row justify-center gap-4 cursor-pointer pb-2 ">
+                <a href={github}>
+                  <FaGithub className="  text-2xl transition-transform duration-300 hover:scale-125 drop-shadow-[0_0_5px_#ffff70]" />
+                </a>
+                <a href={href}>
+                  <FaLink
+                    color="#3190ce"
+                    className="  text-2xl transition-transform duration-300 hover:scale-125 drop-shadow-[0_0_5px_#ffff70]"
+                  />
+                </a>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
